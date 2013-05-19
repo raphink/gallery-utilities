577af39a9f789a7a88931b3f38dc23996d46ab9a
Raphaël Pinson <raphink@gmail.com>
2013-05-19 12:30:07 +0200

    Classify movies

--- autoimport.sh
+++ autoimport.sh
@@ -1,5 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
+# Pictures
 jhead -autorot *.JPG
 jhead -n"%Y-%m-%d/%Y-%m-%d_%H:%M" *.JPG
 
+# Movies
+YEAR=$(date +'%Y')
+while read m d num; do
+  month=$(printf '%02d' $m)
+  dir="${YEAR}-${month}-${d}"
+  file="P${m}${d}${num}.MOV"
+  test -d "$dir" || mkdir $dir
+  mv $file $dir/
+done < <(ls *.MOV | sed -n 's@P\([0-9]\{1,2\}\)\([0-9]\{2\}\)\([0-9]\{4\}\)\.MOV@\1 \2 \3@p')
